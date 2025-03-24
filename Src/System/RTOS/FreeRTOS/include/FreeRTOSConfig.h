34fc1128951d4316fbc2896e563dea318a353238
yjy <3253932102@qq.com>
2025-03-24 15:06:04 +0800

    ⚡️ Perf: 更新freertos配置

--- Src/System/RTOS/FreeRTOS/include/FreeRTOSConfig.h
+++ Src/System/RTOS/FreeRTOS/include/FreeRTOSConfig.h
@@ -70,6 +70,10 @@
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /*-----------------------------------------------------------
  * Application specific definitions.
  *
@@ -77,113 +81,124 @@
  * application requirements.
  *
  * THESE PARAMETERS ARE DESCRIBED WITHIN THE 'CONFIGURATION' SECTION OF THE
- * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE.
+ * FreeRTOS API DOCUMENTATION AVAILABLE ON THE FreeRTOS.org WEB SITE. 
  *
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
-#define vPortSVCHandler SVC_Handler
-#define xPortPendSVHandler PendSV_Handler
+#define vPortSVCHandler     SVC_Handler
+#define xPortPendSVHandler  PendSV_Handler
 #define xPortSysTickHandler SysTick_Handler
 
-// 针对不同的编译器调用不同的stdint.h文件
+//针对不同的编译器调用不同的stdint.h文件
 #if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
 #include <stdint.h>
 extern uint32_t SystemCoreClock;
 #endif
 
-// 断言
-// #define vAssertCalled(char,int) printf("Error:%s,%d\r\n",char,int)
-// #define configASSERT(x) if((x)==0) vAssertCalled(__FILE__,__LINE__) //修改
+//断言
+//#define vAssertCalled(char,int) printf("Error:%s,%d\r\n",char,int)
+//#define configASSERT(x) if((x)==0) vAssertCalled(__FILE__,__LINE__) //修改
 
 /***************************************************************************************************************/
-/*                                        FreeRTOS基础配置配置选项 */
+/*                                        FreeRTOS基础配置配置选项                                              */
 /***************************************************************************************************************/
-#define configUSE_PREEMPTION 1   // 1使用抢占式内核，0使用协程
-#define configUSE_TIME_SLICING 1 // 1使能时间片调度(默认式使能的)
-#define configUSE_PORT_OPTIMISED_TASK_SELECTION                                \
-  1 // 1启用特殊方法来选择下一个要运行的任务
-    // 一般是硬件计算前导零指令，如果所使用的
-    // MCU没有这些硬件指令的话此宏应该设置为0！
-#define configUSE_TICKLESS_IDLE 0            // 1启用低功耗tickless模式
-#define configUSE_QUEUE_SETS 1               // 为1时启用队列
-#define configCPU_CLOCK_HZ (SystemCoreClock) // CPU频率
-#define configTICK_RATE_HZ (1000) // 时钟节拍频率，这里设置为1000，周期就是1ms
-#define configMAX_PRIORITIES (32) // 可使用的最大优先级
-#define configMINIMAL_STACK_SIZE ((unsigned short)130) // 空闲任务使用的堆栈大小
-#define configMAX_TASK_NAME_LEN (16) // 任务名字字符串长度
+#define configUSE_PREEMPTION                    1 //1使用抢占式内核，0使用协程
+#define configUSE_TIME_SLICING                  1 //1使能时间片调度(默认式使能的)
 
-#define configUSE_16_BIT_TICKS 0 // 系统节拍计数器变量数据类型，
-// 1表示为16位无符号整形，0表示为32位无符号整形
-#define configIDLE_SHOULD_YIELD                                                \
-  1 // 为1时空闲任务放弃CPU使用权给其他同优先级的用户任务
-#define configUSE_TASK_NOTIFICATIONS 1 // 为1时开启任务通知功能，默认开启
-#define configUSE_MUTEXES 1            // 为1时使用互斥信号量
-#define configQUEUE_REGISTRY_SIZE 8 // 不为0时表示启用队列记录，具体的值是可以
-// 记录的队列和信号量最大数目。
-#define configCHECK_FOR_STACK_OVERFLOW                                         \
-  0 // 大于0时启用堆栈溢出检测功能，如果使用此功能
+//1启用特殊方法来选择下一个要运行的任务,一般是硬件计算前导零指令
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION 1
+#define configUSE_TICKLESS_IDLE                 0                 //1启用低功耗tickless模式
+#define configUSE_QUEUE_SETS                    1                 //为1时启用队列
+#define configCPU_CLOCK_HZ                      (SystemCoreClock) //CPU频率
+#define configTICK_RATE_HZ                      (1000) //时钟节拍频率，这里设置为1000，周期就是1ms
+#define configMAX_PRIORITIES                    (32)   //可使用的最大优先级
+#define configMINIMAL_STACK_SIZE                ((unsigned short)130) //空闲任务使用的堆栈大小
+#define configMAX_TASK_NAME_LEN                 (16)                  //任务名字字符串长度
+#define configUSE_TASK_FPU_SUPPORT              2 //开启浮点数支持，如果使用浮点数必须开启
+#define configUSE_16_BIT_TICKS                  0 //系统节拍计数器变量数据类型，
+//1表示为16位无符号整形，0表示为32位无符号整形
+
+//为1时空闲任务放弃CPU使用权给其他同优先级的用户任务
+#define configIDLE_SHOULD_YIELD                 1
+#define configUSE_TASK_NOTIFICATIONS            1 //为1时开启任务通知功能，默认开启
+#define configUSE_MUTEXES                       1 //为1时使用互斥信号量
+#define configQUEUE_REGISTRY_SIZE               8 //不为0时表示启用队列记录，具体的值是可以
+//记录的队列和信号量最大数目。
+
+// 大于0时启用堆栈溢出检测功能，如果使用此功能
 // 用户必须提供一个栈溢出钩子函数，如果使用的话
 // 此值可以为1或者2，因为有两种栈溢出检测方法。
-#define configUSE_RECURSIVE_MUTEXES 1  // 为1时使用递归互斥信号量
-#define configUSE_MALLOC_FAILED_HOOK 0 // 1使用内存申请失败钩子函数
-#define configUSE_APPLICATION_TASK_TAG 0
-#define configUSE_COUNTING_SEMAPHORES 1 // 为1时使用计数信号量
+#define configCHECK_FOR_STACK_OVERFLOW          0
+#define configUSE_RECURSIVE_MUTEXES             1 //为1时使用递归互斥信号量
+#define configUSE_MALLOC_FAILED_HOOK            0 //1使用内存申请失败钩子函数
+#define configUSE_APPLICATION_TASK_TAG          0
+#define configUSE_COUNTING_SEMAPHORES           1 //为1时使用计数信号量
 
 /***************************************************************************************************************/
-/*                                FreeRTOS与内存申请有关配置选项 */
+/*                                FreeRTOS与内存申请有关配置选项                                                */
 /***************************************************************************************************************/
-#define configSUPPORT_DYNAMIC_ALLOCATION 1          // 支持动态内存申请
-#define configTOTAL_HEAP_SIZE ((size_t)(10 * 1024)) // 系统所有总的堆大小
+#define configSUPPORT_DYNAMIC_ALLOCATION        1                     //支持动态内存申请
+#define configTOTAL_HEAP_SIZE                   ((size_t)(10 * 1024)) //系统所有总的堆大小
 
 /***************************************************************************************************************/
-/*                                FreeRTOS与钩子函数有关的配置选项 */
+/*                                FreeRTOS与钩子函数有关的配置选项                                              */
 /***************************************************************************************************************/
-#define configUSE_IDLE_HOOK 0 // 1，使用空闲钩子；0，不使用
-#define configUSE_TICK_HOOK 0 // 1，使用时间片钩子；0，不使用
+#define configUSE_IDLE_HOOK                     0 //1，使用空闲钩子；0，不使用
+#define configUSE_TICK_HOOK                     0 //1，使用时间片钩子；0，不使用
 
 /***************************************************************************************************************/
-/*                                FreeRTOS与运行时间和任务状态收集有关的配置选项
- */
+/*                                FreeRTOS与运行时间和任务状态收集有关的配置选项                                 */
 /***************************************************************************************************************/
-#define configGENERATE_RUN_TIME_STATS 0 // 为1时启用运行时间统计功能
-#define configUSE_TRACE_FACILITY 1      // 为1启用可视化跟踪调试
-#define configUSE_STATS_FORMATTING_FUNCTIONS                                   \
-  1 // 与宏configUSE_TRACE_FACILITY同时为1时会编译下面3个函数
-    // prvWriteNameToBuffer(),vTaskList(),
-    // vTaskGetRunTimeStats()
+#define configGENERATE_RUN_TIME_STATS           1 //为1时启用运行时间统计功能
+#define configUSE_TRACE_FACILITY                1 //为1启用可视化跟踪调试
+#define configRECORD_STACK_HIGH_ADDRESS         1 //为1时记录堆栈高地址
+
+// #define portCONFIGURE_TIMER_OF_INT_SOURCE()
+
+// NOTE: wait for setting the timer of interrupt source
+
+// #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS() ()
+// #define portGET_RUN_TIME_COUNTER_VALUE()         ()
+
+//与宏configUSE_TRACE_FACILITY同时为1时会编译下面3个函数
+//prvWriteNameToBuffer(),vTaskList(),
+//vTaskGetRunTimeStats()
+
+#define configUSE_STATS_FORMATTING_FUNCTIONS    1
+/***************************************************************************************************************/
+/*                                FreeRTOS与协程有关的配置选项                                                  */
+/***************************************************************************************************************/
+
+//为1时启用协程，启用协程以后必须添加文件croutine.c
+#define configUSE_CO_ROUTINES                   0
+#define configMAX_CO_ROUTINE_PRIORITIES         (2) //协程的有效优先级数目
 
 /***************************************************************************************************************/
-/*                                FreeRTOS与协程有关的配置选项 */
+/*                                FreeRTOS与软件定时器有关的配置选项                                            */
 /***************************************************************************************************************/
-#define configUSE_CO_ROUTINES                                                  \
-  0 // 为1时启用协程，启用协程以后必须添加文件croutine.c
-#define configMAX_CO_ROUTINE_PRIORITIES (2) // 协程的有效优先级数目
+#define configUSE_TIMERS                        1                          //为1时启用软件定时器
+#define configTIMER_TASK_PRIORITY               (configMAX_PRIORITIES - 1) //软件定时器优先级
+#define configTIMER_QUEUE_LENGTH                5                          //软件定时器队列长度
+
+//软件定时器任务堆栈小
+#define configTIMER_TASK_STACK_DEPTH            (configMINIMAL_STACK_SIZE * 2)
+/***************************************************************************************************************/
+/*                                FreeRTOS可选函数配置选项                                                      */
+/***************************************************************************************************************/
+#define INCLUDE_xTaskGetSchedulerState          1
+#define INCLUDE_vTaskPrioritySet                1
+#define INCLUDE_uxTaskPriorityGet               1
+#define INCLUDE_uxTaskGetStackHighWaterMark     1
+#define INCLUDE_vTaskDelete                     1
+#define INCLUDE_vTaskCleanUpResources           1
+#define INCLUDE_vTaskSuspend                    1
+#define INCLUDE_vTaskDelayUntil                 1
+#define INCLUDE_vTaskDelay                      1
+#define INCLUDE_eTaskGetState                   1
+#define INCLUDE_xTimerPendFunctionCall          1
 
 /***************************************************************************************************************/
-/*                                FreeRTOS与软件定时器有关的配置选项 */
-/***************************************************************************************************************/
-#define configUSE_TIMERS 1 // 为1时启用软件定时器
-#define configTIMER_TASK_PRIORITY (configMAX_PRIORITIES - 1) // 软件定时器优先级
-#define configTIMER_QUEUE_LENGTH 5 // 软件定时器队列长度
-#define configTIMER_TASK_STACK_DEPTH                                           \
-  (configMINIMAL_STACK_SIZE * 2) // 软件定时器任务堆栈大小
-
-/***************************************************************************************************************/
-/*                                FreeRTOS可选函数配置选项 */
-/***************************************************************************************************************/
-#define INCLUDE_xTaskGetSchedulerState 1
-#define INCLUDE_vTaskPrioritySet 1
-#define INCLUDE_uxTaskPriorityGet 1
-#define INCLUDE_vTaskDelete 1
-#define INCLUDE_vTaskCleanUpResources 1
-#define INCLUDE_vTaskSuspend 1
-#define INCLUDE_vTaskDelayUntil 1
-#define INCLUDE_vTaskDelay 1
-#define INCLUDE_eTaskGetState 1
-#define INCLUDE_xTimerPendFunctionCall 1
-
-/***************************************************************************************************************/
-/*                                FreeRTOS与中断有关的配置选项 */
+/*                                FreeRTOS与中断有关的配置选项                                                  */
 /***************************************************************************************************************/
 #ifdef __NVIC_PRIO_BITS
 #define configPRIO_BITS __NVIC_PRIO_BITS
@@ -191,16 +206,60 @@ extern uint32_t SystemCoreClock;
 #define configPRIO_BITS 4
 #endif
 
-#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY 15 // 中断最低优先级
-#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY                           \
-  4 // 系统可管理的最高中断优先级
-#define configKERNEL_INTERRUPT_PRIORITY                                        \
-  (configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
-#define configMAX_SYSCALL_INTERRUPT_PRIORITY                                   \
-  (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY      15 //中断最低优先级
+
+//系统可管理的最高中断优先级
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
+#define configKERNEL_INTERRUPT_PRIORITY \
+    (configLIBRARY_LOWEST_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY \
+    (configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS))
 
 /***************************************************************************************************************/
-/*                                FreeRTOS与中断服务函数有关的配置选项 */
+/*                                FreeRTOS与中断服务函数有关的配置选项                                          */
 /***************************************************************************************************************/
 
+
+//  ━━━━━━━━━━━━━━━━━━━━━━━━━━ Priority values. ━━━━━━━━━━━━━━━━━━━━━━━
+typedef enum {
+    osPriorityNone = 0,              ///< No priority (not initialized).
+    osPriorityIdle = 1,              ///< Reserved for Idle thread.
+    osPriorityLow = 2,               ///< Priority: low
+    osPriorityLow1 = 2 + 1,          ///< Priority: low + 1
+    osPriorityLow2 = 2 + 2,          ///< Priority: low + 2
+    osPriorityLow3 = 2 + 3,          ///< Priority: low + 3
+    osPriorityLow4 = 2 + 4,          ///< Priority: low + 4
+    osPriorityBelowNormal = 7,       ///< Priority: below normal
+    osPriorityBelowNormal1 = 7 + 1,  ///< Priority: below normal + 1
+    osPriorityBelowNormal2 = 7 + 2,  ///< Priority: below normal + 2
+    osPriorityBelowNormal3 = 7 + 3,  ///< Priority: below normal + 3
+    osPriorityBelowNormal4 = 7 + 4,  ///< Priority: below normal + 4
+    osPriorityNormal = 12,           ///< Priority: normal
+    osPriorityNormal1 = 12 + 1,      ///< Priority: normal + 1
+    osPriorityNormal2 = 12 + 2,      ///< Priority: normal + 2
+    osPriorityNormal3 = 12 + 3,      ///< Priority: normal + 3
+    osPriorityNormal4 = 12 + 4,      ///< Priority: normal + 4
+    osPriorityAboveNormal = 17,      ///< Priority: above normal
+    osPriorityAboveNormal1 = 17 + 1, ///< Priority: above normal + 1
+    osPriorityAboveNormal2 = 17 + 2, ///< Priority: above normal + 2
+    osPriorityAboveNormal3 = 17 + 3, ///< Priority: above normal + 3
+    osPriorityAboveNormal4 = 17 + 4, ///< Priority: above normal + 4
+    osPriorityHigh = 22,             ///< Priority: high
+    osPriorityHigh1 = 22 + 1,        ///< Priority: high + 1
+    osPriorityHigh2 = 22 + 2,        ///< Priority: high + 2
+    osPriorityHigh3 = 22 + 3,        ///< Priority: high + 3
+    osPriorityHigh4 = 22 + 4,        ///< Priority: high + 4
+    osPriorityRealtime = 27,         ///< Priority: realtime
+    osPriorityRealtime1 = 27 + 1,    ///< Priority: realtime + 1
+    osPriorityRealtime2 = 27 + 2,    ///< Priority: realtime + 2
+    osPriorityRealtime3 = 27 + 3,    ///< Priority: realtime + 3
+    osPriorityRealtime4 = 27 + 4,    ///< Priority: realtime + 4
+    osPriorityISR = 32,              ///< Reserved for ISR deferred thread.
+    osPriorityError = -1,            ///< System cannot determine priority or illegal priority.
+    osPriorityReserved = 0x7FFFFFFF  ///< Prevents enum down-size compiler optimization.
+} osPriority_t;
+
+#ifdef __cplusplus
+}
+#endif
 #endif /* FREERTOS_CONFIG_H */
