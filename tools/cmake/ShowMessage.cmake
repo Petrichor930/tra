5830dc424070867a2118fda813a82986f190cb93
yjy <3253932102@qq.com>
2025-03-31 18:43:36 +0800

    ✨ Feat(cmake): new cmake config

---
+++ tools/cmake/ShowMessage.cmake
@@ -0,0 +1,16 @@
+function(showMessage)
+  message("Build type:" ${CMAKE_BUILD_TYPE})
+  message( "C Standard: " ${CMAKE_C_STANDARD})
+  message( "C++ Standard: " ${CMAKE_CXX_STANDARD})
+  message( "gcc version: " ${CMAKE_C_COMPILER_VERSION})
+  message( "g++ version: " ${CMAKE_CXX_COMPILER_VERSION})
+  message( "robot version: " ${VERSION})
+  message(
+"    ____   _                  ______             \n"
+"   / __ \\ (_)____   __  __   / ____/____   _____ ___ \n"
+"  / /_/ // // __ \\ / / / /  / /    / __ \\ / ___// _ \\\n"
+" / ____// // / / // /_/ /  / /___ / /_/ // /   /  __/\n"
+"/_/    /_//_/ /_/ \\__, /   \\____/ \\____//_/    \\___/ \n"
+"                 /____/                              \n"
+)
+endfunction()
