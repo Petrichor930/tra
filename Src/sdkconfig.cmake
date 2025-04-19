d9021a62451297aeb7b494f7801089d1c3cab5c9
MYUIN <2812090269@qq.com>
2025-04-19 21:42:12 +0800

    📝 Change to board_c

--- Src/sdkconfig.cmake
+++ Src/sdkconfig.cmake
@@ -1,4 +1,3 @@
 # Board info
-set(CONFIG_TARGET "stm32h723" CACHE STRING "Target Board" FORCE)
-# set(CONFIG_TARGET "stm32f407" CACHE STRING "Target Board" FORCE)
-
+# set(CONFIG_TARGET "stm32h723" CACHE STRING "Target Board" FORCE)
+set(CONFIG_TARGET "stm32f407" CACHE STRING "Target Board" FORCE)
