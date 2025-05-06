aa8a56e77ca21b3485f83127a91ee8c671bb3e22
yjy <3253932102@qq.com>
2025-05-06 19:14:48 +0800

    📈 Chore(cmake): change kconf-frontends to kconfiglib

--- tools/Kconfig/Kconfig.cmake
+++ tools/Kconfig/Kconfig.cmake
@@ -1,19 +1,33 @@
-find_program(KCONFIG_CONF kconfig-conf)
-find_program(KCONFIG_MCONF kconfig-mconf)
+find_program(KCONFIG_DEFCONF alldefconfig)
+find_program(KCONFIG_MCONF menuconfig)
 find_package(Python REQUIRED COMPONENTS Interpreter)
 
-if(KCONFIG_CONF)
-  message(STATUS "Found kconfig-conf: ${KCONFIG_CONF}")
+if(KCONFIG_MCONF)
+  message(STATUS "Found menuconfig: ${KCONFIG_MCONF}")
 else()
-  message(FATAL_ERROR "Could not find kconfig-conf/mconf,\
-                        please install kconfig-frontends")
+  message(FATAL_ERROR "Could not find kconfiglib,\
+                        please use 'pip install kconfiglib' to install it")
 endif()
 
 set(PYTHON_SCRIPT "${CMAKE_SOURCE_DIR}/tools/script/Kconfig2h.py")
 
+# BUG: wait for check update
+# file(TIMESTAMP sdkconfig.h OLD_TIME)
+# file(TIMESTAMP sdkconfig.h NEW_TIME)
+
+add_custom_target(clean_all
+    COMMAND ${CMAKE_COMMAND} -E remove_directory ${CMAKE_BINARY_DIR}/build
+    COMMAND ${CMAKE_COMMAND} -E remove .config
+    COMMENT "Removing build/ and .config"
+)
+
 add_custom_target(menuconfig
   COMMAND ${KCONFIG_MCONF} "${CMAKE_SOURCE_DIR}/Kconfig"
   COMMAND ${Python_EXECUTABLE} ${PYTHON_SCRIPT} "${CMAKE_SOURCE_DIR}/.config"
+  # COMMAND ${CMAKE_COMMAND}
+  #   -DARG1=${OLD_TIME}
+  #   -DARG2=${NEW_TIME}
+  #   -P "${CMAKE_CURRENT_LIST_DIR}/checkUpdate.cmake"
   COMMAND ${CMAKE_COMMAND} -B "${CMAKE_BINARY_DIR}" -G Ninja --fresh
   COMMAND ${CMAKE_COMMAND} -B "${CMAKE_BINARY_DIR}" -G Ninja   # BUG: need to fresh two times
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -21,16 +35,12 @@ add_custom_target(menuconfig
   USES_TERMINAL
 )
 
-if(EXISTS "${CMAKE_BINARY_DIR}")
-  message(STATUS "Found build: ${build_exists}")
-else()
+if(NOT EXISTS "${CMAKE_BINARY_DIR}/build.ninja" OR NOT EXISTS "${CMAKE_SOURCE_DIR}/Src/sdkconfig.h")
   message(WARNING "No found build, Using default config")
 
   # kconfig default config
   execute_process(
-    COMMAND ${KCONFIG_CONF}
-            --defconfig=${CMAKE_SOURCE_DIR}/Kconfig
-            ${CMAKE_SOURCE_DIR}/Kconfig
+    COMMAND ${KCONFIG_DEFCONF} ${CMAKE_SOURCE_DIR}/Kconfig
     COMMAND ${Python_EXECUTABLE} ${PYTHON_SCRIPT} "${CMAKE_SOURCE_DIR}/.config"
     COMMAND -B build --fresh
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -38,20 +48,3 @@ else()
 )
 endif()
 
-file(GLOB CONFIG_FILE "${CMAKE_SOURCE_DIR}/Src/sdkconfig.h")
-if(CONFIG_FILE)
-  message(STATUS "Found config file: ${CONFIG_FILE}")
-else()
-  message(WARNING "No config file found, Using default config")
-
-  # kconfig default config
-  execute_process(
-    COMMAND ${KCONFIG_CONF}
-            --defconfig=${CMAKE_SOURCE_DIR}/Kconfig
-            ${CMAKE_SOURCE_DIR}/Kconfig
-    COMMAND ${Python_EXECUTABLE} ${PYTHON_SCRIPT} "${CMAKE_SOURCE_DIR}/.config"
-    COMMAND -B build --fresh
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    RESULT_VARIABLE kconfig_result
-)
-endif()
