3119a349e4b354ad6b9ebcfe6392fad4c7ca9df5
yjy <3253932102@qq.com>
2025-07-02 21:15:38 +0800

    🐛 Fix: hardfault error(chassis pid nullptr)

--- Src/App/RobotModules/Chassis/Wheel/mecanum.cpp
+++ Src/App/RobotModules/Chassis/Wheel/mecanum.cpp
@@ -31,7 +31,7 @@ void Mecanum::stop()
     currentWheels.M_RB = 0;
 
     for (int i = 0; i < 4; i++) {
-        wheelPID_[i]->reset();
+        // wheelPID_[i]->reset();
     }
 }
 
@@ -91,7 +91,7 @@ void Mecanum::ctrl(ChassisState_s _refState)
     float diff_speed[4] = { 0 };
 
     for (uint8_t i = 0; i < 4; i++) {
-        diff_speed[i] = wheelPID_[i]->calc(wheels_ref.M_RF, currentWheels.M_RF);
+        // diff_speed[i] = wheelPID_[i]->calc(wheels_ref.M_RF, currentWheels.M_RF);
     }
 
     for (uint8_t i = 0; i < 4; i++) {
