1ee2022a904113d7c24c88a784f1d14e022ae714
Petrichor930 <3687402504@qq.com>
2025-08-07 11:23:21 +0800

    Feat: add DM8009 (#7)
    * Feat: add DM8009
    
    * Fix: format
    
    * Fix: delete

---
+++ Src/Common/Extensions/Motor/Core/Imp/DAMIAO/DM8009/DM8009.cpp
@@ -0,0 +1,74 @@
+#include "DM8009.hpp"
+
+#include "DMMotor.hpp"
+
+#include "StmLog.hpp"
+
+#include <cstring>
+
+using namespace PINYMOTOR;
+using namespace DMMOTOR;
+DM8009::DM8009(const char _name[16], InitConfig_s _config)
+        : DMMotor(_name, std::move(_config))
+{
+    LOG::CHECK(checkBaseConfig());
+
+
+    strcpy(this->model_.name, "DM-DM8009");
+    this->model_.measureMax = 16383;
+    this->model_.measureMin = 0;
+    this->model_.reductionRatio = 1.f;
+    this->model_.rxBaseId = 0x10;
+    this->model_.txBaseId = 0x00;
+
+    this->status_ = Status_s(P_MAX,             // PMax
+                             V_MAX,             // VMax
+                             T_MAX,             // TMax
+                             MIT_KP_MAX,        // MITKpMax
+                             MIT_KD_MAX,        // MITKdMax
+                             CURR_TX_CODE_SPAN, // currTxCodeSpan
+                             0.f,               // 临时占位，实际需补充真实值
+                             0.f,               // 临时占位，实际需补充真实值
+                             CURR_MAX,          // currMax
+                             TORQ_MAX,          // torqMax
+                             TORQ_CONSTANT      // torqConstant
+    );
+
+    this->registerMotor();
+    this->registerRecvCallback();
+    this->updateCtrlId();
+
+    LOG::info("DM8009",
+              " %s: An instance of DM8009 created, rxBaseId:%hx, txBaseId:%hx",
+              this->name_, this->model_.rxBaseId, this->model_.txBaseId);
+    // TODO:
+}
+
+MotorTypeDef_e DM8009::checkBaseConfig()
+{
+    MotorTypeDef_e rslt = 0;
+
+    if (this->comType_ != ComType_e::FDCAN &&
+        this->comType_ != ComType_e::CAN) {
+        rslt |= 1;
+        LOG::error("DM8009", " %s: only support FDCAN or CAN comtype",
+                   this->name_);
+    }
+
+    if (this->workMode_ == WorkMode_e::QUAD_VOLT) {
+        rslt |= 1;
+        LOG::error("DM8009", " %s: WorkMode is not supported", this->name_);
+    }
+
+    if (this->offsetId_ > 9) {
+        rslt |= 1;
+        LOG::error("DM8009", " %s: Max Offset ID is only 9!", this->name_);
+    }
+
+    if (this->txFreq_ > 1000) {
+        rslt |= 1;
+        LOG::error("DM8009", " %s: Max TxFreq is only 1000!", this->name_);
+    }
+
+    return rslt;
+}
---
+++ Src/Common/Extensions/Motor/Core/Imp/DAMIAO/DM8009/DM8009.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include "../DMMotor.hpp"
+
+namespace PINYMOTOR::DMMOTOR {
+
+class DM8009 final : public DMMotor {
+private:
+    MotorTypeDef_e checkBaseConfig();
+
+public:
+    static constexpr float OMEGA_MAX = 125.9685f;
+    static constexpr float P_MAX = 12.5f;
+    static constexpr float V_MAX = 45.f;
+    static constexpr float T_MAX = 54.f;
+    static constexpr float MIT_KP_MAX = 1.f;
+    static constexpr float MIT_KD_MAX = 4.f;
+    static constexpr float CURR_TX_CODE_SPAN = 10000.f;
+    static constexpr float CURR_MAX = 41.044777f; // DM上位机看电机上电打印反馈
+    static constexpr float TORQ_MAX = 40.f;
+    // 关于转矩电流与转矩关系
+    // https://blog.csdn.net/sy243772901/article/details/82925212
+    static constexpr float MAG_FLUX = 0.0044f;     // 磁通量
+    static constexpr float NP = 21.f;              // 极对数
+    static constexpr float INTER_RR = (9.f / 1.f); // 内部减速比
+    static constexpr float TORQ_CONSTANT = 0.9745f;
+
+    DM8009(const char _name[16], InitConfig_s _config);
+};
+} // namespace PINYMOTOR::DMMOTOR
