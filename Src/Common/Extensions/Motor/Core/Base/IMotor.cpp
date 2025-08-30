38be0a5ebe69316cc60242a492a4fcd443b93c83
Myuin <85868520+Minhyuin@users.noreply.github.com>
2025-08-30 21:19:08 +0800

    fix(PINYMOTOR): fix setZeroAng(float) (#64)

--- Src/Common/Extensions/Motor/Core/Base/IMotor.cpp
+++ Src/Common/Extensions/Motor/Core/Base/IMotor.cpp
@@ -257,7 +257,8 @@ void IMotor::setZeroAng()
 void IMotor::setZeroAng(float _zeroAng)
 {
     // TODO: setZeroAng need a semaphore to protect
-    data_.zeroAng = this->isReverse_ ? (2.f * PI) - _zeroAng : _zeroAng;
+    // _zeroAng is a rawAng in "current reverse situation"
+    data_.zeroAng = _zeroAng;
 }
 float IMotor::getCmdCurr()
 {
