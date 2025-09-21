f740ede6b104ba0d712c1fb226fbbf5c2cc7fe97
yjy <3253932102@qq.com>
2025-09-21 14:01:59 +0800

    chore(soc): add extern handler in main.h after stm32cubemx gen

--- Src/Common/Soc/stm32h723/genSoc.sh
+++ Src/Common/Soc/stm32h723/genSoc.sh
@@ -2,9 +2,9 @@
 
 rm -rf ./hal/Drivers
 
-mkdir -p hal/Drivers/CMSIS/Device/ST/STM32F4xx
-cp -r ../../../../ThirdParty/CMSIS_Device_F4/Include/ hal/Drivers/CMSIS/Device/ST/STM32F4xx
-cp -r ../../../../ThirdParty/STM32F4xx_HAL_Driver/ hal/Drivers
+mkdir -p hal/Drivers/CMSIS/Device/ST/STM32H7xx
+cp -r ../../../../ThirdParty/CMSIS_Device_H7/Include/ hal/Drivers/CMSIS/Device/ST/STM32H7xx
+cp -r ../../../../ThirdParty/STM32H7xx_HAL_Driver/ hal/Drivers
 cp -r ../../../../ThirdParty/CMSIS5/Core/Include/ hal/Drivers/CMSIS/
 
 MAIN_C_FILE="hal/Core/Src/main.c"
